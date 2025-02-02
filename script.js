beeeea6e7786f6bac26c7b734d288fd9840fec37
turingAround <120207535+turingAround@users.noreply.github.com>
2025-02-02 14:17:28 -0700

    function to create computer's choice

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-console.log (`Test`);
-
+const getComputerChoice = function() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    
+    switch(randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+console.log(getComputerChoice());
