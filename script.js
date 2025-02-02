920da114f979b2631ef27489caef0fb5ce893ecd
turingAround <120207535+turingAround@users.noreply.github.com>
2025-02-02 14:41:52 -0700

    created function to play game

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const getComputerChoice = function() {
+// create computer's pick.
+   
+function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     
     switch(randomNumber) {
@@ -10,6 +12,8 @@ const getComputerChoice = function() {
             return "scissors";
     }
 }
+
+// Function to get user's pick.
 
 function getHumanChoice() {
     const choice = prompt("Please enter Rock, Paper, or Scissors:");
@@ -32,5 +36,28 @@ function getHumanChoice() {
     }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+// Initialize scores
+
+let humanScore = 0;
+let computerScore = 0;
+
+// Function to play a round of the game.
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    } else {
+        computerScore++;
+        return `You lose! ${computerChoice} beats ${humanChoice}`;
+    }
+}
+
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
